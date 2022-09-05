930a9bf2fc2fa1b2a87b0a6ec6d8fea739bb7411
NVK PAVANKUMAR <npavankumar144@gmail.com>
2022-09-05 13:31:29 +0530

    locators updated for logini page

--- cypress/pageobjects/LoginPage.js
+++ cypress/pageobjects/LoginPage.js
@@ -1,20 +1,20 @@
 class LoginPage {
   fillEmail(value) {
-    const emailAdress = cy.get("#txtUsername");
+    const emailAdress = cy.get("input[name='username']");
     emailAdress.clear();
     emailAdress.type(value);
     return this;
   }
 
   fillPassword(value) {
-    const Password = cy.get("#txtPassword");
+    const Password = cy.get("input[name='password']");
     Password.clear();
     Password.type(value);
     return this;
   }
 
   clickSignIn() {
-    const Signinbtn = cy.get("#btnLogin");
+    const Signinbtn = cy.get("button[type='submit']");
     Signinbtn.click();
     return this;
   }
